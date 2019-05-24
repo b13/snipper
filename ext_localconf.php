e3553d2f94d876f6920c2b339b9de78b6a6ce2cc
David Steeb <david.steeb@b13.com>
2019-05-24 16:29:13 +0000

    [BUGFIX] Use correct namespace for Hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc'][] = \B13\Amppages\Hooks\TypoLinkHandler::class . '->postProcessTypoLink';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc'][] = \B13\Snipper\Hooks\TypoLinkHandler::class . '->postProcessTypoLink';
