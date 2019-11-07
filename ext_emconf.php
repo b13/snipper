8611447dca0126cd4f5bfb6eea6483d2bad5d035
David Steeb <david.steeb@b13.de>
2019-11-07 11:30:13 +0100

    [TASK] Release v1.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 
 $EM_CONF['snipper'] = [
     'title' => 'Snipper ',
-    'description' => 'Keep external links secure by adding rel="noopener" to all external typlinks',
+    'description' => 'Keep external links secure by adding rel="noopener" to all external typolinks',
     'category' => 'fe',
     'author' => 'David Steeb',
     'author_email' => 'typo3@b13.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author_company' => 'b13 GmbH, Stuttgart',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'constraints' => [
         'depends' =>
             [
