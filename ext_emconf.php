c56f91902a155778f156bf0f2e9879e388a2d0ac
Lisa Kreitz <lisa.kreitz@b13.com>
2025-12-04 15:11:29 +0100

    [TASK] Set correct TYPO3 version for dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['snipper'] = [
     'constraints' => [
         'depends' =>
             [
-                'typo3' => '12.4.0-13.99.99',
+                'typo3' => '12.4.0-14.99.99',
             ],
     ],
 ];
